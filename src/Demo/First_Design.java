2882b53c1e2cad333996b87e02c1367b108f4dbc
Sharath-Gowda1 <sharathgowda3272@gmail.com>
2024-06-26 09:55:31 +0530

    First design by B architect

--- src/Demo/First_Design.java
+++ src/Demo/First_Design.java
@@ -5,6 +5,8 @@ public class First_Design {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Design 1 by A");
+		System.out.println("Design 1 by B");
+
 	}
 
 }

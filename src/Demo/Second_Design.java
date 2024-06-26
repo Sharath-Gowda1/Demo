cf69c4748b0851b0218282170eea08079899f450
Sharath-Gowda1 <sharathgowda3272@gmail.com>
2024-06-26 10:05:24 +0530

    second design by B

--- src/Demo/Second_Design.java
+++ src/Demo/Second_Design.java
@@ -5,6 +5,8 @@ public class Second_Design {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("second Design BY A");
+		System.out.println("second Design BY B");
+
 	}
 
 }

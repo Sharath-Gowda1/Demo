895846f0532e99cedcf65b791b05a498b65a6471
Sharath-Gowda1 <sharathgowda3272@gmail.com>
2024-06-26 09:30:56 +0530

    Second test by b

--- src/Demo/Second_test.java
+++ src/Demo/Second_test.java
@@ -5,6 +5,8 @@ public class Second_test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("second test from A");
+		
+		System.out.println("second test from B");
 	}
 
 }
